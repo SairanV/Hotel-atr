60db0ba6410675732ef13bd58ec3a2d4b506dba0
aidyn <aidynmaskutov2004@gmail.com>
2024-02-12 10:35:53 +0600

    dfsdf

--- Hotel-atr/Controllers/RoomController.cs
+++ Hotel-atr/Controllers/RoomController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Hotel_atr.Models;
 
 namespace Hotel_atr.Controllers
 {
     public class RoomController : Controller
     {
+        private IWebHostEnvironment host;
+
+        public RoomController(IWebHostEnvironment host)
+        {
+            this.host = host;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -19,10 +28,26 @@ namespace Hotel_atr.Controllers
             return View();
         }
 
+
+        //string email, name
+        //User user
+        //Request.Form
         [HttpPost]
-        public IActionResult Subcribes(string email)
+        public IActionResult Subcribes(IFormFile userFile)
         {
+            string path = Path.Combine(host.WebRootPath, 
+                "files", 
+                userFile.FileName);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                userFile.CopyTo(stream);
+            }
+
             return View();
         }
+
+
+
     }
 }
---
+++ Hotel-atr/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Hotel_atr.Models
+{
+    public class User
+    {
+        public string email { get; set; }
+        public string name{ get; set; }
+
+    }
+}
